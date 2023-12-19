724b84f19974eb1bdc316b122c354413b97b0442
taikool2121996 <taikool2121996@gmail.com>
2023-12-20 04:51:09 +0700

    Stop tracking *.glb with Git LFS

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -78,14 +78,6 @@ export function Island({ isRotating, setIsRotating, ...props }) {
             material={materials.SF_Windows_Mat}
           />
           <mesh
-            geometry={nodes.Final_Bridge1_SF_Windows_Mat_0_2.geometry}
-            material={materials.SF_Windows_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Bush_Mat_0.geometry}
-            material={materials.SF_Bush_Mat}
-          />
-          <mesh
             geometry={nodes.Final_Bridge1_SF_TreeWood_Mat_0.geometry}
             material={materials.SF_TreeWood_Mat}
           />
@@ -112,18 +104,6 @@ export function Island({ isRotating, setIsRotating, ...props }) {
           <mesh
             geometry={nodes.Final_Bridge1_SF_CutRock_Mat_0.geometry}
             material={materials.SF_CutRock_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Stalagmites_Mat_0.geometry}
-            material={materials.SF_Stalagmites_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_WoodTex_Mat_0.geometry}
-            material={materials.SF_WoodTex_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_PlasterTex_Mat_0.geometry}
-            material={materials.SF_PlasterTex_Mat}
           />
           <mesh
             geometry={nodes.Final_Bridge1_SF_StoneBrick_Mat_0.geometry}
@@ -164,50 +144,6 @@ export function Island({ isRotating, setIsRotating, ...props }) {
           <mesh
             geometry={nodes.Final_Bridge1_SF_GrassCards_Mat_0_2.geometry}
             material={materials.SF_GrassCards_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_GrassCards_Mat_0_3.geometry}
-            material={materials.SF_GrassCards_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Sack_Mat_0.geometry}
-            material={materials.SF_Sack_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_MineCart_Mat_0.geometry}
-            material={materials.SF_MineCart_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Bridge_Mat_0.geometry}
-            material={materials.SF_Bridge_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Door_Mat_0.geometry}
-            material={materials.SF_Door_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Water_Mat_0.geometry}
-            material={materials.SF_Water_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_Final_Water001_0.geometry}
-            material={materials['Final_Water.001']}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_Final_Rocks2_Mat_0.geometry}
-            material={materials.Final_Rocks2_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_Black_0.geometry}
-            material={materials.Black}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_ButterFly_Mat_0.geometry}
-            material={materials.SF_ButterFly_Mat}
-          />
-          <mesh
-            geometry={nodes.Final_Bridge1_SF_Lillypad_Mat_0.geometry}
-            material={materials.SF_Lillypad_Mat}
           />
         </group>
       </group>
